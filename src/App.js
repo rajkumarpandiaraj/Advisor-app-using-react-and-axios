de4d4079a0bad4cdbcd601f92402b0d8add18c01
rajkumar <rajkumarpandiyaraj@gmail.com>
2021-01-02 08:13:05 -0800

    advisor app using react and axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'; 
 
 function App() {
+  const [ advice, setAdvice ] = useState('')
+  useEffect(() =>{
+    getAdvice()
+  },[])
+  const getAdvice  = () =>{
+    axios.get('https://api.adviceslip.com/advice')
+    .then(res =>{
+      setAdvice(res.data.slip.advice)
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='container'>
+          <div className='inner-text'>
+            <p>{advice}</p>
+            <button onClick={getAdvice} className='btn'>Get Another</button>
+          </div>
+        </div>
     </div>
   );
 }
